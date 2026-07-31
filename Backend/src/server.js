80d3ea9d254b0e9cab0112b4372e9ddd7f2cb945
anuja <anujaadhikari24@gmail.com>
2026-07-31 19:09:21 +0545

    commit

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,20 +1,20 @@
-import express from "express"
+import express, {json} from "express"
 import dotenv from "dotenv"
 
 
 const app= express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World")
+app.get("/doctors", (req, res) => {   ///all doctors
+    res.send("welcome to expressjs").status(200).json({message:"appointment created"})
 })
 
 app.get("/student", (req, res) => {
 res.json({
-    name: "John Doe",
-     age: 20, 
-     course: "Computer Science"
-    })
-
+    
+    id:1,
+    name:"Anuja",
+    course:"MERN stack"
+   })
 })
 
 app.post("/create", (req, res)=>{
@@ -29,10 +29,20 @@ app.put("/appointments/:ID", (req, res)=>{
 app.delete("/appointments/:ID", (req, res)=>{
     res.send("Appointment deleted successfully")
 })
+app.get("/doctors/:id",(req,res)=>{   // single get garnu xa vane yo roue url use garxan
+    res.send(req.params.id)
+})
+     // multiple parametres (/student/id /Anuja)
 
+app.get("/appointments/:id/:name",(req,res)=>{
+    res.json(req.params)
+})
+// query parameters uses in pagination
+app.get("/doctors",(req,res)=>{
+    res.json(req.query)
+})
 
 const PORT = process.env.PORT || 3000;
-
 
 
 app.listen(PORT, ()=>{
