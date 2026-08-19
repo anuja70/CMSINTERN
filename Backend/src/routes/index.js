f97a94e96f0657699edce4abfc76f1ba9329a6ee
anuja <anujaadhikari24@gmail.com>
2026-08-19 08:38:00 +0545

    update the codebase

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,19 +1,26 @@
 import express from "express"
-import authRoutes from "../module/auth.route.js"
-import adminRoutes from "../module/module/admin.routes.js"
+import authRoutes from "../module/auth/auth.routes.js"
+import adminRoutes from "../module/auth/admin.routes.js"
 import patientRoutes from "../module/patient/patient.routes.js"
+import doctorRoutes from "../module/doctor/doctor.routes.js"
+import appointmentRoutes from "../module/appointment/appointent.routes.js"
 
 const router = express.Router();
 
 router.get("/health",(req,res)=>{
     return res.json({
-        message:"clinic management system",
+        message:"cilnic managemnet system",
         success:true
     })
 })
 router.use("/auth",authRoutes)
 router.use("/admin",adminRoutes)
 router.use("/patient",patientRoutes)
+router.use("/doctor",doctorRoutes)
+router.use("/appointment",appointmentRoutes)
+
+
+
 
 
 
