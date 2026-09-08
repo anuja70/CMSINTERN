298a58e62feba3d82b583f582b836423461d1a45
anuja <anujaadhikari24@gmail.com>
2026-09-08 18:21:28 +0545

    khalti configuration

--- Backend/src/config/env.js
+++ Backend/src/config/env.js
@@ -5,7 +5,6 @@ dotenv.config();
 export const env = {
     // Server
     PORT: process.env.PORT || 3000,
-    DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV || "development",
    // Database
   DATABASE_URL: process.env.DATABASE_URL,
