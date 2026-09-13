b69458a0b57516d003e37b4021d8efe28a7fe1bb
anuja <anujaadhikari24@gmail.com>
2026-09-13 19:30:00 +0545

    fixing errors

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  root: 'Frontend',
   plugins: [
     react(),
     tailwindcss(),
